6b22469daea3e8a555a0a89c0cabb2a3b91b9779
Josh Padilla <jdp237@nau.edu>
2020-10-25 14:24:59 -0700

    adding necessary files

---
+++ server.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const  path = require("path");
+
+// Sets up the Express App
+// =============================================================
+const app = express();
+const PORT = 3000;
+
+// Sets up the Express app to handle data parsing
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
