2e2044cee9b6bf0e8c00854fe5d2cc5c26cd1218
Josh Padilla <jdp237@nau.edu>
2020-10-25 15:29:41 -0700

    added HTML and API routes

--- server.js
+++ server.js
@@ -9,3 +9,27 @@ const PORT = 3000;
 // Sets up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
+
+//HTML Routes
+app.get("/", function (req, res) {
+    res.sendFile(path.join(__dirname, "/public/index.html"));
+});
+
+app.get("/notes", function (req, res) {
+    res.sendFile(path.join(__dirname, "/public/notes.html"));
+});
+
+
+//API Routes
+app.get("/api/notes", function(req, res) {
+    fs.readFile("/db/db.json", function (err, data) {
+        if (err) throw (err);
+        let notes = JSON.parse(data)
+        return res.json(notes);
+    })
+});
+
+app.listen(PORT, function () {
+    console.log("App listening on PORT, http://localhost:" + PORT);
+});
