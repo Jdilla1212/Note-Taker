eb770ccecf1e16332b07fb8971cd5e9e50f8c094
Josh Padilla <jdp237@nau.edu>
2020-10-29 12:15:48 -0700

    finished README.md

--- README.md
+++ README.md
@@ -1,1 +1,27 @@
-# Note-Taker
+## Express Note-Taker
+![License badge](https://img.shields.io/badge/license-MIT-green)
+
+## Description
+An express-driven application to view, add and delete notes.  Using front and back-end development to power this app, there are HTML and API routes  
+
+## Table of Contents
+
+* [Installation](#installation)
+* [Usage](#usage)
+* [Contribution](#contribution)
+
+## Installation Instructions
+Install Node, express, and npm packages to your local machine and clone the repository.
+
+## Usage
+Open the local server through your node command-line and then use the application in your browser with the localhost address.
+
+## Contribution Guidelines
+Feel free to contribute to this project as you find fitting.
+
+## Questions
+How to contact me:
+
+Email: jdp237@nau.edu
+
+Github.com/jdilla1212
--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const  path = require("path");
 const fs = require('fs');
+const shortid = require('shortid');
+let db = require("./db/db.json");
 
 // Sets up the Express App
 // =============================================================
@@ -10,6 +12,7 @@ const PORT = 3000;
 // Sets up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(express.static("public"));
 
 
 //HTML Routes
@@ -30,6 +33,30 @@ app.get("/api/notes", function(req, res) {
         return res.json(notes);
     })
 });
+
+//POST routes
+app.post("/api/notes", function(req, res){
+    let newNote = {
+        id: shortid.generate(),
+        title: req.body.title,
+        text: req.body.text,
+    };
+    console.log(newNote);
+    db.push(newNote);
+    fs.writeFile("./db/db.json", JSON.stringify(db), (err, data) => {
+        if (err) throw (err);
+        return res.json(db);
+    });
+});
+
+app.delete("/api/notes/:id", (req, res) => {
+    db = db.filter((note) => note.id !== req.params.id);
+    console.log(db);
+    fs.writeFile("./db/db.json", JSON.stringify(db), (err, data) => {
+        if (err) throw (err);
+        res.sendStatus(200);
+    });
+})
 
 app.listen(PORT, function () {
     console.log("App listening on PORT, http://localhost:" + PORT);
